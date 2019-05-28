41e43b689b1df43670ac51d32f457b39fa81c3b3
Hirae <32942962+hiraetari@users.noreply.github.com>
2019-05-28 12:52:33 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/superstackker.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/superstackker.dir/main.cpp.obj"
+  "superstackker.pdb"
+  "superstackker.exe"
+  "superstackker.exe.manifest"
+  "libsuperstackker.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/superstackker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+using namespace std;
+
+template <typename T>
+class Stack {
+private:
+    T top;
+    int capacity;
+    T *stack;
+
+public:
+    Stack(T capacity) {
+        if (capacity <= 0)
+            cout << "Stack must have 1 or more elements." << endl;
+        stack = new T[capacity];
+        this -> capacity = capacity;
+        top = -1;
+    }
+
+    void push(T value) {
+        if (top == capacity)
+            cout << "Stack is full." << endl;
+        top++;
+        stack[top] = value;
+    }
+
+    bool pop() {
+        if (!isEmpty()){
+	    --top;
+	    return true;
+        }
+	else
+            cout << "Stack is empty." << endl;
+	    return false;
+    }
+
+    T peek() {
+        if (isEmpty())
+            cout << "Stack is empty." << endl;
+        return stack[top];
+    }
+
+    bool isEmpty() {
+        return (top == -1);
+    }
+
+    void print() {
+        if (!isEmpty()) {
+            for (int i = top; i >= 0; --i)
+                cout << stack[i] << endl;
+        }
+        else
+            cout << "Stack is empty." << endl;
+    }
+};
+
+int main() {
+    Stack<int> myStack1(4);
+    Stack<char> myStack2(2);
+
+    myStack1.push(1);
+    myStack1.push(2);
+    myStack1.push(2408);
+    myStack1.push(100000000);
+    myStack1.print();
+
+    myStack1.pop();
+    cout << "Top element is: " << myStack1.peek() << endl;
+    myStack1.pop();
+    cout << "Top element is: " << myStack1.pop() << endl;
+    myStack1.print();
+    myStack1.pop();
+    myStack1.print();
+
+    myStack2.push('a');
+    myStack2.push('b');
+    myStack2.print();
+    cout << "Top element is: " << myStack2.peek() << endl;
+    myStack2.pop();
+    myStack2.print();
+
+    return 0;
+}
